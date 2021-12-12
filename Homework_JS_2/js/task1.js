d4076ca74b579a329fe2fcb3951b0fba8aecd7a6
Gelik <gelik@tut.by>
2021-12-13 00:09:46 +0300

    Task_1

---
+++ Homework_JS_2/js/task1.js
@@ -0,0 +1,12 @@
+let name = prompt("Ваше имя",);
+let age = prompt("Ваш возраст",);
+let city = prompt("город проживания",);
+let phone = prompt("телефон",);
+let email = prompt("Электронная почта",);
+let company = prompt("Место работы",);
+
+
+
+alert('Меня зовут ' + name + '. Мне ' + age + '. Я проживаю в ' + city + ' и работаю в компании ' + company + '. Мои контактные данные: '+ phone + ' '+email);
+window.name = name;
+window.age = age;
---
+++ Homework_JS_2/js/task2.js
@@ -0,0 +1,4 @@
+let Year =  new Date();
+Year = Year.getFullYear();
+let BirthYear = Year-window.age;
+document.write (window.name + ' родился в ' + BirthYear + ' году.');
